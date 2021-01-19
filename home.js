73dd69112f1d9df7abec103d2d9b0407f06eef6d
RomainGe <58071634+RomainGe@users.noreply.github.com>
2021-01-19 15:10:36 +0100

    ajout duplication de code

--- home.js
+++ home.js
@@ -32,6 +32,41 @@ function FilterLastName() {
       } else {
         tr[i].style.display = "none";
       }
-    }       
+    }
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }          
   }
+}
+
+function FilterLastName() {
+  var input, filter, table, tr, td, i, txtValue;
+input = document.getElementById("prenom");
+filter = input.value.toUpperCase();
+table = document.getElementById("myTable");
+tr = table.getElementsByTagName("tr");
+for (i = 0; i < tr.length; i++) {
+  td = tr[i].getElementsByTagName("td")[1];
+  if (td) {
+    txtValue = td.textContent || td.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      tr[i].style.display = "";
+    } else {
+      tr[i].style.display = "none";
+    }
+  }
+  if (td) {
+    txtValue = td.textContent || td.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      tr[i].style.display = "";
+    } else {
+      tr[i].style.display = "none";
+    }
+  }          
+}
 }
